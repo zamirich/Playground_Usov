c27b163689d3b5bd636bfd03f6b5e126e486c6b5
ZamiRich <argashokovzamir@gmail.com>
2021-01-07 01:11:28 +0100

    Worked through the page #173

--- Usov2.playground/Pages/5 - Loops.xcplaygroundpage/Contents.swift
+++ Usov2.playground/Pages/5 - Loops.xcplaygroundpage/Contents.swift
@@ -1,4 +1,5 @@
 //: [Previous](@previous)  | Page 5 - Loops | [Next Page](@next)
+import Foundation
 /*:
  ---
  - Callout(Page 159): For-in
@@ -25,7 +26,6 @@ for (countryName, blockName) in countriesAndAllies {
     print("\(countryName) is in \(blockName)")
 }
 print()
-
 //: ### In case we don't need values, but only keys, or vice versa:
 for (countryName, _) in countriesAndAllies {
     print("\(countryName)")
@@ -54,6 +54,97 @@ for (team, results) in resultsOfGames {
     }
 }
 print()
+/*:
+ ---
+ - Callout(Page 164): While
+ */
+//: * while
+var i = 1
+while i == 2 { //this returns false and the code does not even start
+    print("\(i) - from while")
+    i += 1
+}
+//: * repeat-while
+var u = 1
+repeat {
+    print("\(u) - from while-repeat") //this code runs in the beginning regardless of the condition, and only then condition checked
+    u += 1
+} while u == 2
+print()
+//: ### Continue
+for i in 1...10 {
+    if i % 2 == 0 {
+        continue //basically it forces to start another iteration in the loop ignoring any other code after this word
+        print("hello") //this code will not be executed
+    } else {
+        print(i)
+    }
+}
+print()
+//: ### Break
+for i in 1...999 {
+    let randomNumber = Int(arc4random_uniform(10))
+    print(randomNumber)
+    if randomNumber == 2 {
+        print("Iteration number \(i)")
+        break //so once the statement is true, this command will fully stop the loop and quit it without ending other iterations
+    }
+}
+//: ### Breaking a main loop from nested loop:
+mainLoop: for i in 1...5 {
+    for y in i...5 {
+        if y == 4 && i == 2 {
+            break mainLoop
+        }
+        print("\(i) - \(y)")
+    }
+}
+print()
+/*:
+ ---
+ - Callout(Page 168): #HW
+ */
+//: ## Task 1
+//we have 3 students who attended some lessons and received certain marks
+var attendanceList: [String:[String:UInt]] = [:]
+attendanceList["Ivanov"] = ["1 Jan 2021":3, "4 Jan 2021":5, "7 Jan 2021":4, "10 Jan 2021":3]
+attendanceList["Petrov"] = ["1 Jan 2021":2, "4 Jan 2021":2, "7 Jan 2021":2, "10 Jan 2021":3]
+attendanceList["Sidorov"] = ["1 Jan 2021":5, "4 Jan 2021":5, "7 Jan 2021":5, "10 Jan 2021":3]
 
-//last page 163
+//calculating average mark per each student
+for (student, _) in attendanceList {
+    var totalMark: Float = 0
+    for (_, mark) in attendanceList[student]! {
+        totalMark += Float(mark)
+    }
+    totalMark /= Float(attendanceList[student]!.count)
+    print("\(student) has average of \(totalMark)")
+}
+print()
+
+//calculating average mark of all students
+var totalMark: Float = 0
+var numberOfMarks: Float = 0
+for (student , _) in attendanceList {
+    for (_ , mark) in attendanceList[student]! {
+        totalMark += Float(mark)
+        numberOfMarks += 1
+    }
+}
+print("Students have average of \(totalMark/numberOfMarks)\n")
+//: ## Task 2
+//creating an alias for a chess figure with name and it's optional position on the board
+typealias Chessman = Dictionary<String,(alpha:Character,num:Int)?>
+
+//creating a dictionary with 3 figures, one of which is taken/killed:
+var chessmen: Chessman = ["King":(alpha:"E",num:1),"Queen":(alpha:"D",num:1),"Bishop":nil]
+
+//creating a loop that checks whether every figure is on the board or taken
+for (figure, _) in chessmen {
+    if chessmen[figure]! == nil {
+        print("\(figure) was killed.")
+    } else {
+        print("\(figure) is positioned at \(chessmen[figure]!!.alpha)\(chessmen[figure]!!.num)")
+    }
+}
 //: [Previous](@previous)  |  [Next Page](@next)
---
+++ Usov2.playground/Pages/6 - Functions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+//: [Previous](@previous)  | Page 6 - Functions | [Next Page](@next)
+import Foundation
+/*:
+ ---
+ - Callout(Page 169): Functions
+ */
+//last page is 173
+//: ### How to use for-in with a decreasing sequence of numbers:
